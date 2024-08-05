e2be7aef574d8aeaf158a8569040ddaa41e83dbb
FALKERN <83498803+Falkern@users.noreply.github.com>
2024-08-04 17:28:40 -0700

    on scroll animations
    i cooked hard

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const elements = document.querySelectorAll('.section__text__p1, .title, .section__text__p2, .about-pic, .details-container, .about-me-text');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('fade-in');
+    } else {
+      entry.target.classList.remove('fade-in');
+    }
+  });
+}, { threshold: 0.1 });
+
+elements.forEach(element => {
+  observer.observe(element);
+});
